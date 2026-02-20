eb5cd8f265370c6d6677eb372ef7c3e6250c59b6
krishnamanglik4 <krishnamanglik2504@gmail.com>
2026-02-21 00:10:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+// var num = 10;
+// console.log(num);
+// console.log(typeof(num));
+
+// num = false;
+// console.log(num);
+// console.log(typeof(num));
+
+// num = "krishna";
+// console.log(num);
+// console.log(typeof(num));
+
+// num = null;
+// console.log(num);
+// console.log(typeof(num));
+
+// num;
+// console.log(num);
+// console.log(typeof(num));
+
+
+// const sym1 = Symbol("4");
+// console.log(sym1);
+// const sym2 = Symbol("4");
+// console.log(sym2);
+// if(sym1==sym2){
+//     console.log("true");
+// }
+
+// else{
+//     console.log("false");
+// }
+
+
+
+
+
+
+
+var a = 100;
+var b = 13;
+var c = 10;
+var linebreak ="<br>";
+
+document.write("a+b+c=");
+result = a+b+c;
+document.write(result);
+document.write(linebreak);
+
+document.write("a*b*c=");
+result = a+b+c;
+document.write(result);
+document.write(linebreak);
+
+
+document.write("a-b-c=");
+result = a+b+c;
+document.write(result);
+document.write(linebreak);
+
