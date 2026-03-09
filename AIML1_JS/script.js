b84f3cd1bdde7a4887b055241b7ef33d4b803d3b
krishnamanglik4 <krishnamanglik2504@gmail.com>
2026-03-09 21:06:06 +0530

    Fix nested repo issue

---
+++ AIML1_JS/script.js
@@ -0,0 +1,249 @@
+
+// var num = 10;
+// console.log(num);
+// console.log(typeof(num));
+
+// num = false;
+// console.log(num);
+// console.log(typeof(num));
+
+// num = "krishna";
+// console.log(num);
+// console.log(typeof(num));
+
+// num = null;
+// console.log(num);
+// console.log(typeof(num));
+
+// num;
+// console.log(num);
+// console.log(typeof(num));
+
+
+// const sym1 = Symbol("4");
+// console.log(sym1);
+// const sym2 = Symbol("4");
+// console.log(sym2);
+// if(sym1==sym2){
+//     console.log("true");
+// }
+
+// else{
+//     console.log("false");
+// }
+
+
+
+
+
+
+
+// var a = 100;
+// var b = 13;
+// var c = 10;
+// var linebreak ="<br>";
+
+// document.write("a+b+c=");
+// result = a+b+c;
+// document.write(result);
+// document.write(linebreak);
+
+// document.write("a*b*c=");
+// result = a+b+c;
+// document.write(result);
+// document.write(linebreak);
+
+
+// document.write("a-b-c=");
+// result = a+b+c;
+// document.write(result);
+// document.write(linebreak);
+
+
+
+
+
+// var a = 10;
+// var b = 20;
+
+// var linebreak = "<br/>";
+
+// document.write("(a==b)=>");
+// result = (a==b);
+// document.write(result);
+// document.write(linebreak);
+
+// document.write("(a>b)=>");
+// result = (a>b);
+// document.write(result);
+// document.write(linebreak);
+
+
+// document.write("(a<b)=>");
+// result = (a<b);
+// document.write(result);
+// document.write(linebreak);
+
+
+// document.write("(a!=b)=>");
+// result = (a!=b);
+// document.write(result);
+// document.write(linebreak);
+
+// document.write("(a<=b)=>");
+// result = (a<=b);
+// document.write(result);
+// document.write(linebreak);
+
+// document.write("(a>=b)=>");
+// result = (a>=b);
+// document.write(result);
+// document.write(linebreak);
+
+
+
+// var count ;
+// document.write("starting loop" + "<br/>");
+// for( count = 0 ; count < 10 ; count++){
+//     document.write("Current Count:" + count);
+//     document.write("<br/>")
+// }
+
+// document.write("Loop Stopped");
+
+
+
+// const  person = {
+//     firstname : "Ajay",
+//     lastname : "Singh",
+//     age : 25
+// };
+// console.log("Personal details: " );
+// for(let x in person){
+//     console.log( x + " is : " + person[x])
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var num = 10;
+// console.log(num);
+// console.log(typeof(num));
+
+// num = false;
+// console.log(num);
+// console.log(typeof(num));
+
+// num = "krishna";
+// console.log(num);
+// console.log(typeof(num));
+
+// num = null;
+// console.log(num);
+// console.log(typeof(num));
+
+// num;
+// console.log(num);
+// console.log(typeof(num));
+
+
+// const sym1 = Symbol("4");
+// console.log(sym1);
+// const sym2 = Symbol("4");
+// console.log(sym2);
+// if(sym1==sym2){
+//     console.log("true");
+// }
+
+// else{
+//     console.log("false");
+// }
+
+
+
+
+
+
+
+// var a = 100;
+// var b = 13;
+// var c = 10;
+// var linebreak ="<br>";
+
+// document.write("a+b+c=");
+// result = a+b+c;
+// document.write(result);
+// document.write(linebreak);
+
+// document.write("a*b*c=");
+// result = a+b+c;
+// document.write(result);
+// document.write(linebreak);
+
+
+// document.write("a-b-c=");
+// result = a+b+c;
+// document.write(result);
+// document.write(linebreak);
+
+
+
+
+
+
+// var myinfo = new Array();
+// console.log(myinfo);
+// var myinfo1 = Array();
+// console.log(myinfo1);
+
+
+
+
+// var trainerinfo = ["Jitender" , 29 , "Ghaziabad" , (ismarried = true)];
+
+// console.log(trainerinfo[trainerinfo.length - 4]);
+// console.log(trainerinfo[trainerinfo.length - 3]);
+// console.log(trainerinfo[trainerinfo.length - 2]);
+// console.log(trainerinfo[trainerinfo.length - 1]);
+
+
+// trainerinfo.contact = 7614986749;
+
+// trainerinfo[0] = "Jitendra kumar chauhan";
+// console.log(trainerinfo.contact);
+// console.log(trainerinfo[trainerinfo.length - 4]);
+
+
+
+
+// DOM Manipulation in JS
+
+// const result = document.getElementById("para");
+// result.innerHTML = "My Self";
+// result.title = "myname";
+
+// const result2 = document.getElementsByClassName("hii")[0];
+// result2.innerHTML = "HELLO!";
+
+// const result3 = document.getElementsByClassName("hii")[1];
+// result3.innerHTML = "HOW ARE YOU!";
+
+
+
+
+
+
+
+
+
